ac6db7cddc23e2f68aeab1d65340ae0700b56a82
hassan <hassanak509@gmail.com>
2025-03-08 23:03:39 +0200

    created the signup.php api

---
+++ article-server/apis/v1/signup.php
@@ -0,0 +1,37 @@
+<?php
+require("../../connection/connection.php");
+require('../../models/user.php');
+require('../../utils/functions.php');
+
+if (!issets_post_data("full_name", "email", "pass")) {
+    http_response_code(400);
+    echo json_encode([
+        "message" => "full name, email and password are required"
+    ]);
+    exit();
+}
+
+$full_name = $_POST["full_name"];
+$email = $_POST["email"];
+$pass = hash('sha256', $_POST["pass"]);
+
+try {
+    $user = User::createUser($db_connect, $full_name, $email, $pass);
+
+    if ($user) {
+        $message = 'success';
+    } else {
+        $message = 'Something went wrong try again';
+        $user = null;
+    }
+
+    echo json_encode([
+        [
+            'message' => $message,
+            'user' => $user
+        ]
+    ]);
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(["message" => $e->getMessage()]);
+}
