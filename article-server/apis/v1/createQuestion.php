c1061355ff3e4eeb3fd9a38fa343be1c3b82f6b9
hassan <hassanak509@gmail.com>
2025-03-09 17:00:28 +0200

    created the createQuestion api

---
+++ article-server/apis/v1/createQuestion.php
@@ -0,0 +1,37 @@
+<?php
+require("../../connection/connection.php");
+require('../../models/question.php');
+require('../../utils/functions.php');
+
+if (!issets_post_data("id", "question", "answer")) {
+    http_response_code(400);
+    echo json_encode([
+        "message" => "question and answer are required"
+    ]);
+    exit();
+}
+
+$user_id = $_POST["id"];
+$question = $_POST["question"];
+$answer = $_POST["answer"];
+
+try {
+    $question = Question::createQuestion($db_connect, $user_id, $question, $answer);
+
+    if ($question) {
+        $message = 'success';
+    } else {
+        $message = 'Something went wrong try again';
+        $question = null;
+    }
+
+    echo json_encode([
+        [
+            'message' => $message,
+            'question' => $question
+        ]
+    ]);
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(["message" => $e->getMessage()]);
+}
