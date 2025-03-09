60c4a64f86ac48b3da053b43057cf9e1b956807f
hassan <hassanak509@gmail.com>
2025-03-09 16:59:58 +0200

    created the question model

--- article-server/models/Question.php
+++ article-server/models/Question.php
@@ -2,6 +2,24 @@
 
 class Question
 {
+    public static function createQuestion($db_connect, $user_id, $question, $answer)
+    {
+        $query = $db_connect->prepare("insert into questions(user_id, question, answer) values (?, ?, ?)");
+        $query->bind_param("iss", $user_id, $question, $answer);
+        $query->execute();
+
+        if ($query->affected_rows > 0) {
+            $question = [
+                "question_id" => $db_connect->insert_id,
+                "user_id" => $user_id,
+                "question" => $question,
+                "answer" => $answer
+            ];
+            return $question;
+        } else {
+            throw new Exception("Failed to create FAQ.");
+        }
+    }
     public static function getQuestions($db_connect)
     {
         $query = $db_connect->prepare("SELECT * FROM Questions");
